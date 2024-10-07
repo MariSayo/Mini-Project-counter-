fa3ce1bbb43c338dbcc99af53f45c2c1b982537d
Maria <maria.g1611@yahoo.com>
2024-10-07 08:32:03 +0100

    commit 1

---
+++ index.js
@@ -0,0 +1,32 @@
+//document.getElementById("count-el").innerText = 5
+//we need a way to store the count. 
+//Variables store the data like let, const etc
+
+//  initiialize the count as 0
+// listen for clicks on the increment button
+// increment the count variable when the button is clicked
+// change the count-el in the HTML to reflect the new count
+
+
+//define onClick in html
+
+// function increment() {
+//     //body of the function
+//     //Every time function is called the button reacts
+//     console.log("The button was clicked")
+// }
+
+// function lifeanddeath(){
+//     return 42
+// }
+// console.log(lifeanddeath())
+
+let lap1 = 34
+let lap2 = 33
+let lap3 = 36
+
+function Totalrace(){
+    return lap1 + lap2 + lap3
+}
+
+console.log(Totalrace())
